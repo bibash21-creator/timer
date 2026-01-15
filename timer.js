aa4b7df354880d52bd6d3a8c3cf96fbf500a7537
bibash21-creator <bibashworkmail21@gmail.com>
2026-01-15 13:03:00 +0545

    Added files

---
+++ timer.js
@@ -0,0 +1,57 @@
+// Function to run the interactive timer properly
+let timerDuration = 0;
+let remainingTime = 0;
+let intervalId = null;
+let sessionCount= 0;
+
+const display = document.getElementById('timer-dis');
+const sessionCounter = document.getElementById('session-count');
+const alertSound = document.getElementById('alert-sound')
+
+
+function updateDisplay(){
+
+}
+
+function startTimer(){
+
+}
+
+function pauseTimer(){
+
+}
+
+function resetTimer(){
+
+}
+
+function setDuration(minutes){
+
+}
+
+function completeTimer(){
+
+}
+
+// Event Listeners
+
+// Start buttons
+document.getElementById('start-btn').addEventListener('click', 
+    startTimer()
+)
+
+// Pause buttons
+document.getElementById('pause-btn').addEventListener('click', )
+
+// Reset buttons
+document.getElementById('reset-btn').addEventListener('click', )
+
+// Preset buttons
+document.getElementById('').addEventListener('click', )
+
+
+// Theme Toggle 
+document.getElementById('theme-toggle').addEventListener('click', )
+
+// Set Custom Time
+document.getElementById('set-custom').addEventListener('click', )
