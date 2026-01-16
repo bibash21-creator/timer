9ae54fb74db79254366fc877ef912ab361910897
bibash21-creator <bibashworkmail21@gmail.com>
2026-01-16 21:30:37 +0545

    Added timer function

--- timer.js
+++ timer.js
@@ -1,15 +1,13 @@
-// Function to run the interactive timer properly
-let timerDuration = 0;
-let remainingTime = 0;
-let intervalId = null;
-let sessionCount= 0;
+// Javascript functions goes here
 
-const display = document.getElementById('timer-dis');
-const sessionCounter = document.getElementById('session-count');
-const alertSound = document.getElementById('alert-sound')
+// Variables
+workDuration = 0;
+shortBreak = 0;
+longBreak = 0;
+currentMode = [];
+timerInterval = null
 
-
-function updateDisplay(){
+function addTask(){
 
 }
 
@@ -25,33 +23,23 @@ function resetTimer(){
 
 }
 
-function setDuration(minutes){
+// Mode switching
+function switchMode(mode){
+    currentMode = mode;
 
 }
 
-function completeTimer(){
+// Ai Focus Coah
+function showQuote(){
 
 }
 
-// Event Listeners
+// Daily velocity
+var totalFocusMinues = 0;
+var streakDays = 0;
+recentActivity = [];
 
-// Start buttons
-document.getElementById('start-btn').addEventListener('click', 
-    startTimer()
-)
+function updateVelocity(){
+    
+}
 
-// Pause buttons
-document.getElementById('pause-btn').addEventListener('click', )
-
-// Reset buttons
-document.getElementById('reset-btn').addEventListener('click', )
-
-// Preset buttons
-document.getElementById('').addEventListener('click', )
-
-
-// Theme Toggle 
-document.getElementById('theme-toggle').addEventListener('click', )
-
-// Set Custom Time
-document.getElementById('set-custom').addEventListener('click', )
